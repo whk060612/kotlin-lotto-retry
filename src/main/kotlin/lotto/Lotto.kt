0e00f44e19fbe35dc54cbaa2410887a9961a27cb
whk06061 <gju06061@naver.com>
2022-11-28 16:54:50 +0900

    feat: 로또 번호 중복 검사하는 기능 구현

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -1,8 +1,11 @@
 package lotto
 
+import lotto.data.LOTTO_NUMBER_COUNT
+
 class Lotto(private val numbers: List<Int>) {
     init {
-        require(numbers.size == 6)
+        require(numbers.size == LOTTO_NUMBER_COUNT)
+        require(numbers.toSet().size == LOTTO_NUMBER_COUNT)
     }
 
     // TODO: 추가 기능 구현
