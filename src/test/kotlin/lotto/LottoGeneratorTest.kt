c2cc943dd1f3714aa5dd0cc3a930f22090ce0dcf
whk06061 <gju06061@naver.com>
2022-11-28 16:50:39 +0900

    test: 구입 금액 만큼 로또를 발행하는 기능 테스트

---
+++ src/test/kotlin/lotto/LottoGeneratorTest.kt
@@ -0,0 +1,17 @@
+package lotto
+
+import org.junit.jupiter.api.Test
+import org.assertj.core.api.Assertions.assertThat
+
+class LottoGeneratorTest {
+
+    private var count = 0
+
+    @Test
+    fun `받은 금액 만큼 로또를 발행한다`() {
+        count = 5
+        val lottoGenerator = LottoGenerator(count)
+        val result = lottoGenerator.generate()
+        assertThat(result.size).isEqualTo(count)
+    }
+}
