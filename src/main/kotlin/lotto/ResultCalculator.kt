5a9c73f3f3ab9cba79df63fa33e05590418e67fd
whk06061 <gju06061@naver.com>
2022-11-28 20:27:42 +0900

    feat: 수익률을 계산하는 기능 구현

--- src/main/kotlin/lotto/ResultCalculator.kt
+++ src/main/kotlin/lotto/ResultCalculator.kt
@@ -1,6 +1,7 @@
 package lotto
 
 import lotto.data.*
+import kotlin.math.roundToInt
 
 class ResultCalculator {
 
@@ -9,6 +10,12 @@ class ResultCalculator {
         SECOND_AND_THIRD_CORRECT_COUNT -> if (bonusNumberCorrect) Rank.SECOND else Rank.THIRD
         FOURTH_CORRECT_COUNT -> Rank.FOURTH
         else -> Rank.FIFTH
+    }
+
+    fun calculateYieldRate(prize: Int, buyMoney: Int):Double {
+        val yieldRate = prize.toDouble() / buyMoney.toDouble() * 100.0
+        val result = (yieldRate * 10.0).roundToInt() / 10.0
+        return result
     }
 
 }
